2402751fe8f8b29886a2131ff6ea88a0b638da7e
feitosaedu35 <160509388+feitosaedu35@users.noreply.github.com>
2024-06-05 09:43:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal= document.querySelector(".caixa-principal")
+const caixaPerguntas= document.querySelector(".caixa-perguntas")
+const caixaAlternativas= document.querySelector(".caixa-alternativas")
+const caixaResultado= document.querySelector(".caixa-resultado")
+const textoResultado= document.querySelector(".texto-resultado")
+
+const perguntas =[
+{
+    enunciado : "pergunta1"
+    alternativas: "Alternativa1", "Alternativa2"
+},{
+    enunciado : "pergunta2"
+    alternativas: "Alternativa1", "Alternativa2"
+},
+]
